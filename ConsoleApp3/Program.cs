72de2ee7f1713e53750e6bf710df98e7ef2d321d
BE-23 <sotomayorrohit.sjb@gmail.com>
2026-09-23 11:55:35 -0500

    primer proyecto guardado

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double CalcularAreaCirculo(double radio)
+            {
+                return 3.1415 * radio * radio;
+                
+            }
+            Console.WriteLine(CalcularAreaCirculo(6));
+        }
+    }
+}
